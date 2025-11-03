cae336ebc5578cdd96ff5312b3934efed895851a
RanarTheWrathful <143533828+RanarTheWrathful@users.noreply.github.com>
2025-11-03 04:39:04 -0500

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     // Game server domain.
     // If the host is 'localhost:NUMBER', the NUMBER must be the port setting.
-    host: "https://pp-updates.onrender.com/#loc",
+    host: "https://pp-updates.onrender.com",
     // Which port to run the web server on.
     port: 3000,
     // Server
