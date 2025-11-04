5a61dd61d6db750d8ee92db750531a5f8859923d
RanarTheWrathful <143533828+RanarTheWrathful@users.noreply.github.com>
2025-11-04 03:58:19 -0500

    Update ppTanks.js

--- server/lib/definitions/tankAddons/ppTanks.js
+++ server/lib/definitions/tankAddons/ppTanks.js
@@ -2,10 +2,9 @@
 // This is helpful, if your group relies on all other definitions already being loaded.
 // Addons that are dependant on other addons should be named something like
 // "[PARENT ADDON NAME]-[EXTENSION NAME].js", to make sure that it would run after that addon ran.
-const { makeRarities, makePresent, makeRelic, makeCrasher, makeLaby, menu, combineStats, skillSet, addAura, makeDeco, makeAuto, makeHybrid, makeOver, makeTurret, weaponArray, dereference, LayeredBoss, newWeapon, makeRadialAuto, setTurretProjectileRecoil, makeMulti } = require('../facilitators.js');
+const { makeRarities, makePresent, makeRelic, makeCrasher, makeLaby, menu, combineStats, skillSet, addAura, makeDeco, makeAuto, makeGuard, makeBird, makeHybrid, makeOver, makeTurret, weaponArray, dereference, LayeredBoss, newWeapon, makeRadialAuto, setTurretProjectileRecoil, makeMulti } = require('../facilitators.js');
 const { smshskl, base, gunCalcNames, basePolygonDamage, basePolygonHealth, dfltskl, statnames} = require('../constants.js');
 const g = require('../gunvals.js');
-
 Class.ppTanks = menu("Prophecy Paradox Tanks")
 Class.pptanks.UPGRADES_TIER_0 = []
 Class.addons.UPGRADES_TIER_0.push("ppTanks");
