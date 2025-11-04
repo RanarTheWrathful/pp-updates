9b9f61fd155d35c6a42f69f8ecc474ded7128690
RanarTheWrathful <143533828+RanarTheWrathful@users.noreply.github.com>
2025-11-04 05:52:58 -0500

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,16 +3,19 @@ module.exports = {
 
     // Game server domain.
     // If the host is 'localhost:NUMBER', the NUMBER must be the port setting.
-    host: "pp-updates.onrender.com",
+     host: "localhost:3000",
+
     // Which port to run the web server on.
     port: 3000,
+
     // Server
+
     // How often to update the list of the entities that players can see.
     // Has effects of when entities are activated.
     visibleListInterval: 250,
 
     // Start up logs and Log speed loop warnings
-    LOGS: false,
+    LOGS: true,
 
     // If set to true, it loads all mockups and doesn't needs to generate while ingame. If set to false, does require need to generate mockups ingame but starts the main server instantly.
     LOAD_ALL_MOCKUPS: false,
@@ -24,10 +27,10 @@ module.exports = {
             // This is important and usefull,
             // if your VM (Your machine that hosts the website stuff) doesn't support multi ports,
             // you are forced to set this to true because it will load via through the main server.
-            LOAD_ON_MAINSERVER: true,
+            LOAD_ON_MAINSERVER: false,
             // This is very important where the host is actually being hosted. Keep in mind that the ports gets changed to 3000, to 3001, and it repeats with the other servers.
             // Keep in mind that if "LOAD_ON_MAINSERVER" is set to true then this code will be replaced with the main server's host instead.
-            HOST: "pp-updates.onrender.com",
+            HOST: "localhost:3001",
             // The port where to host from.
             // Keep in mind that if "LOAD_ON_MAINSERVER" is set to true then this code will be replaced with the main server's port instead.
             PORT: 3001,
