5cd9ce8889a088a7fba299494799d083856eb30e
RanarTheWrathful <143533828+RanarTheWrathful@users.noreply.github.com>
2025-11-04 04:13:20 -0500

    Update and rename pp Bosses to pp Bosses.js

--- server/lib/definitions/tankAddons/pp Bosses.js
+++ server/lib/definitions/tankAddons/pp Bosses.js
@@ -2,7 +2,7 @@
 // This is helpful, if your group relies on all other definitions already being loaded.
 // Addons that are dependant on other addons should be named something like
 // "[PARENT ADDON NAME]-[EXTENSION NAME].js", to make sure that it would run after that addon ran.
-//THIS IS THE BOSSES ADDON FOR PROPHECY PARADOX
+//THIS IS THE BOSSES ADDON FOR PROPHECY PARADOX.
 const { makeRarities, makePresent, makeRelic, makeCrasher, makeLaby, menu, combineStats, skillSet, addAura, makeDeco, makeAuto, makeGuard, makeBird, makeHybrid, makeOver, makeTurret, weaponArray, dereference, LayeredBoss, newWeapon, makeRadialAuto, setTurretProjectileRecoil, makeMulti } = require('../facilitators.js');
 const { smshskl, base, gunCalcNames, basePolygonDamage, basePolygonHealth, dfltskl, statnames} = require('../constants.js');
 const g = require('../gunvals.js');
