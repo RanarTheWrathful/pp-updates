81994e4b7d63a0ded7e55ccfdab939466365676d
RanarTheWrathful <143533828+RanarTheWrathful@users.noreply.github.com>
2025-11-04 03:52:55 -0500

    Update ppTanks.js

--- server/lib/definitions/tankAddons/ppTanks.js
+++ server/lib/definitions/tankAddons/ppTanks.js
@@ -2,7 +2,7 @@
 // This is helpful, if your group relies on all other definitions already being loaded.
 // Addons that are dependant on other addons should be named something like
 // "[PARENT ADDON NAME]-[EXTENSION NAME].js", to make sure that it would run after that addon ran.
-const { menu, combineStats, skillSet, addAura, makeDeco, makeAuto, makeHybrid, makeOver, weaponArray, dereference, makeMulti } = require('../facilitators.js');
+const { menu, combineStats, skillSet, addAura, makeDeco, makeAuto, makeHybrid, makeOver, makeTurret, weaponArray, dereference, makeMulti } = require('../facilitators.js');
 const { smshskl, base, gunCalcNames, basePolygonDamage, basePolygonHealth, dfltskl, statnames} = require('../constants.js');
 const g = require('../gunvals.js');
 
