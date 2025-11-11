33b4b2d800b896a18a487669ecf7d5f612944628
RanarTheWrathful <143533828+RanarTheWrathful@users.noreply.github.com>
2025-11-11 17:47:47 -0500

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,3 +1,11 @@
+let serverMode = "render";
+if (serverMode === "render) {
+    config.LOAD_ON_MAINSERVER = true;
+//config.host = "localhost:3000";
+    } else if (serverMode === "local") {
+    config.LOAD_ON_MAINSERVER = false;
+        config.host = "localhost:3000";
+}
 module.exports = {
     // Server host
 
