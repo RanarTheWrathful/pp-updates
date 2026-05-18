d43b3e242605173f4dec07456bfd4664ff765a40
RanarTheWrathful <143533828+RanarTheWrathful@users.noreply.github.com>
2026-05-18 07:57:09 -0400

    Update index.js

--- server/Game/index.js
+++ server/Game/index.js
@@ -459,6 +459,7 @@ class gameHandler {
             let CC = Class[o.defs[0]];
             if (!CC) CC = {};
             o.controllers = [];
+            if (o.FACING_TYPE !== "autospin") o.FACING_TYPE = "looseToTarget";
             o.define({
                 CONTROLLERS: CC.CONTROLLERS ? [...Class.bot.CONTROLLERS, ...CC.CONTROLLERS] : Class.bot.CONTROLLERS,
                 FACING_TYPE: CC.FACING_TYPE ? CC.FACING_TYPE : Class.bot.FACING_TYPE,
