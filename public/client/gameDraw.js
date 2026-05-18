0c4987e7f9d83075851666f75ec6fc39161a193d
RanarTheWrathful <143533828+RanarTheWrathful@users.noreply.github.com>
2026-05-18 07:22:11 -0400

    Update gameDraw.js

--- public/client/gameDraw.js
+++ public/client/gameDraw.js
@@ -212,10 +212,10 @@ slow = 0.5 + 0.5 * Math.sin(t * 0.6),
 breath = 0.5 + 0.5 * Math.sin(t * 1.8),
 
 // micro instability (jitters the feeling)
-glitch = (Math.sin(t * 17.3) * Math.sin(t * 3.7)) * Math.random()*10,
+glitch = (Math.sin(t * 17.3) * Math.sin(t * 3.7)) * 0.08,
 
 // combine
-pulse = slow * 0.55 + breath * 0.35 + glitch;
+pulse = (slow * 0.55 + breath * 0.35 + glitch) * (Math.random() * 2);
         
         gameDraw.animatedColor.lesbian = gameDraw.getRainbow(lesbian_useSecondSet ? lesbian_oredange : lesbian_white, lesbian_useSecondSet ? lesbian_white : lesbian_magenta, (lesbian_useSecondSet ? five_bars : five_bars - 3) / 2);
         gameDraw.animatedColor.gay = gameDraw.hslToRgb(gay_transition, 0.75, 0.5);
