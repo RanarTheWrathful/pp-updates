45f59d7b2a12ab9f96703dbd3defe497615735b0
RanarTheWrathful <143533828+RanarTheWrathful@users.noreply.github.com>
2026-05-18 04:28:56 -0400

    Update gameDraw.js

--- public/client/gameDraw.js
+++ public/client/gameDraw.js
@@ -200,8 +200,8 @@ var gameDraw = {
             trans_blue = "#55cdfc",
             trans_white = "#ffffff";
         
-            abyssRain1 = "#666666",
-            abyssRain2 = "#363636",
+            abyssRain1 = "#ffffff",
+            abyssRain2 = "#666666",
             abyssRain3 = "#000000",
         
         pulse = (Math.sin(now / 1000) + 1) / 2;
@@ -210,9 +210,8 @@ var gameDraw = {
         gameDraw.animatedColor.gay = gameDraw.hslToRgb(gay_transition, 0.75, 0.5);
         gameDraw.animatedColor.trans = gameDraw.mixColors(trans_white, 2000 > now % 4000 ? trans_blue : trans_pink, Math.max(Math.min(5 * Math.sin(now % 2000 / 2000 * Math.PI) - 2, 1), 0)); // Animated!
         
-
-gameDraw.animatedColor.abyss =
-    gameDraw.mixColors(abyssRain3, 2000 > now % 4000 ? abyssRain2 : abyssRain1, pulse);
+gameDraw.animatedColor.abyss = gameDraw.mixColors(abyssRain3, 2000 > now % 4000 ? abyssRain2 : abyssRain1, Math.max(Math.min(5 * Math.sin(now % 2000 / 2000 * Math.PI) - 2, 1), 0)); // Animated!
+        
         gameDraw.animatedColor.magenta = gameDraw.hslToRgb(
             light_purple.h + (purple.h - light_purple.h) * ratio,
             light_purple.s + (purple.s - light_purple.s) * ratio,
